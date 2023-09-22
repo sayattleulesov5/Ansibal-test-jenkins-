f20cf3c154d412b61192eb84cf3e9fe9503a2e7a
sayattleulesov5 <73901359+sayattleulesov5@users.noreply.github.com>
2023-09-22 11:47:59 +0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,3 +2,4 @@ PORT=22 && docker run -d --rm --name local-vps-$PORT -p $PORT:$PORT -p 80:80 atl
 ssh-copy-id root@127.0.0.1
 ssh-keygen -R 127.0.0.1
 ssh-keygen -R localhost
+server@@127.0.0.1's password: password
